b430134b99048d03278dfc20baadeee228dc8f3a
Yuriy Pilipenko <yurik2061@gmail.com>
2023-08-18 16:00:08 +0300

    logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,7 @@
+import {  iconLogo,  } from "../../images/icons";
 
+
+import { StyledLogo } from "../Button/Button.styled";
 import { Container } from "../Container/Container";
 
 
@@ -6,7 +9,8 @@ import { Container } from "../Container/Container";
 const App =() => {
   return (
   <Container>
-
+    <StyledLogo>{iconLogo}</StyledLogo>
+      <h2> React </h2>
 
   </Container>
   );  
