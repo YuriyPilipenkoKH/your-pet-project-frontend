53e0dc53bf75017604da5a04326b4bebd37dd391
Yuriy Pilipenko <yurik2061@gmail.com>
2023-08-18 18:55:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename = "your-pet-project-frontend" >
       <App />
     </BrowserRouter>
   </React.StrictMode>
