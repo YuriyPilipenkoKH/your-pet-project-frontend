c2f6ffa2585bf32fc6d55c19452da2322ede4b79
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-08-21 14:56:26 +0300

    fix logo

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,8 +1,8 @@
 
 import { AuthNavWrap, LayoutWrap,  SharedLayoutWrap } from "./SharedLayout.styled";
 
-import { iconLogo } from "../../images/icons";
-import { Suspense } from "react";
+import { iconLogo, iconLogoBig } from "../../images/icons";
+import { Suspense, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { AuthNav } from "../AuthNav/AuthNav";
 import { ButtonBurger } from "../Nav/Nav.styled";
@@ -11,10 +11,24 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 import { StyledLogo } from "../Button/Button.styled";
 
 export const SharedLayout = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const isMobile = windowWidth <= 768;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <SharedLayoutWrap className="sharedLayout">
       <LayoutWrap className="layoutWrap">
-        <StyledLogo to="/">{iconLogo}</StyledLogo>
+        <StyledLogo to="/">{isMobile ? iconLogo : iconLogoBig}</StyledLogo>
         <Nav/>
         <AuthNavWrap>
           <AuthNav/>
