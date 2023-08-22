cb4e562400819f2f09204690f6861505772d61cf
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-08-22 18:09:23 +0300

    add page for login and register

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginForm from '../components/Forms/LoginForm/LoginForm'
+
+export default function LoginPage() {
+  return (
+    <div>
+        <LoginForm />
+    </div>
+  )
+}
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RegisterForm from '../components/Forms/RegisterForm/RegisterForm'
+
+export default function RegisterPage() {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  )
+}
