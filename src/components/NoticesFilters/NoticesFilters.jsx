905beaabb397de0621071a91dd15a98776e8ae67
Nikolas82 <nikobogdan34@gmail.com>
2023-08-22 20:15:24 +0300

    add card

--- src/components/NoticesFilters/NoticesFilters.jsx
+++ src/components/NoticesFilters/NoticesFilters.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { AddToButton, ButtonTransparent, FavButton } from '../Button/Button';
-import { iconFilter, iconHeart } from '../../images/icons';
+import { iconFilter } from '../../images/icons';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { FilterWrapper } from './NoticesFilters.styled';
 
 export default function NoticesFilters() {
-    const styles = {
-        button: {
-          display: 'none',
-        },
-        buttonLarge: {
-            display: 'flex',
-        },
-    };
+   
 
     return (
         <FilterWrapper 
--- src/pages/NoticesPage.jsx
+++ src/pages/NoticesPage.jsx
@@ -3,6 +3,13 @@ import NoticesSearch from '../components/NoticesSearch/NoticesSearch'
 import NoticesCategoriesNav from '../components/NoticesCategoriesNav/NoticesCategoriesNav'
 import NoticesFilters from '../components/NoticesFilters/NoticesFilters'
 import { NoticesPageWrap } from './pages.styled/NoticesPage.styled'
+import { MainCard } from '../components/MainCard/MainCard'
+import { NewsCard } from '../components/News/NewsCard'
+import news from '../components/json/news.json';
+
+
+
+
 
 export default function NoticesPage() {
   return (
@@ -12,6 +19,14 @@ export default function NoticesPage() {
       <NoticesCategoriesNav/>
       <NoticesFilters/>
       </NoticesPageWrap>
+      <MainCard className="main-card">
+        {news.map((item, index) => (
+          <NewsCard
+            key={index}
+            
+          />
+        ))}
+      </MainCard>
       </>
   )
 }
