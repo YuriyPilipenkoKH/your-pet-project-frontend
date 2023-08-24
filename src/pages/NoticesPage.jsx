a06369fdb33e956865f958b5199a7ebaf0135f90
Yuriy Pilipenko <yurik2061@gmail.com>
2023-08-24 16:54:42 +0300

    Update NoticesPage.jsx

--- src/pages/NoticesPage.jsx
+++ src/pages/NoticesPage.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import NoticesSearch from '../components/NoticesSearch/NoticesSearch'
 import NoticesCategoriesNav from '../components/NoticesCategoriesNav/NoticesCategoriesNav'
 import NoticesFilters from '../components/NoticesFilters/NoticesFilters'
 import { NoticeContainer, NoticesPageWrap } from './pages.styled/NoticesPage.styled'
 import { MainCard } from '../components/MainCard/MainCard'
-import news from '../utils/json/news.json';
+
 import { CommonWrapper } from './pages.styled/Pages.styled'
 import { getNoticesList } from 'redux/notices/notices-selectors'
 import { useDispatch, useSelector } from 'react-redux';
@@ -16,21 +16,13 @@ export default function NoticesPage() {
   const dispatch = useDispatch();
 const noticesList = useSelector(getNoticesList)
 console.log(noticesList);
-const [notices, setNotices] = useState([]);
-  
-// useEffect(() => {
-//   setNotices(noticesList);
-// }, [noticesList]);
 
-//   useEffect(() => {
-//     dispatch(noticesOperations.fetchAllNotices()).then((data)=>setNotices(data));
-// }, [dispatch, noticesList]);
 
 useEffect(() => {
   dispatch(noticesOperations.fetchAllNotices())
     .then((data) => {
       console.log(data);
-      setNotices(data);
+      // setNotices(data);
     })
     .catch((error) => {
       console.error('Error fetching notices:', error);
