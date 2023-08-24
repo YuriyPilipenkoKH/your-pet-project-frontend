1c6a4b7bb1796467a3ab4a860e92f1d0ab5acf96
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-08-24 18:23:56 +0300

    da

--- src/components/Forms/AddPetForm/AddInGoodHandsPet/MoreInfo/MoreInfo.jsx
+++ src/components/Forms/AddPetForm/AddInGoodHandsPet/MoreInfo/MoreInfo.jsx
@@ -112,7 +112,7 @@ export default function MoreInfo({
         deliveryDataPet({
             comments: coment,
             petAvatar: image,
-            category: 'my ads',
+            category: 'in good hands',
             location,
             sex: active,
         });
