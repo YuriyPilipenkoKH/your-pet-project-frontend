0ab7f9b844883032f3ca29e2878c3265e6820d8e
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-08-25 11:02:02 +0300

    add pagination

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,16 +3,16 @@ import { BsSearch } from 'react-icons/bs';
 import { NewsCard } from '../components/News/NewsCard';
 import news from '../utils/json/news.json';
 import {
-  ListButtonForPagination,
-  NewsContainer,
-  NewsWrapper,
-  PaginationButton,
-  PaginationWrapper,
-  SearchForm,
-  SearchIcon,
-  SearchInput,
-  SearchWrapper,
-  TytleNwes,
+    ListButtonForPagination,
+    NewsContainer,
+    NewsWrapper,
+    PaginationButton,
+    PaginationWrapper,
+    SearchForm,
+    SearchIcon,
+    SearchInput,
+    SearchWrapper,
+    TytleNwes,
 } from './pages.styled/Pages.styled';
 import { StyledLink } from '../components/Button/Button.styled';
 import { FormButton } from 'components/Button/Button';
@@ -23,9 +23,8 @@ export const NewsPage = () => {
     const [itemOffset, setItemOffset] = useState(0);
     const cardsPerPage = 6;
     const endOffset = itemOffset + cardsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
     const currentItems = news.slice(itemOffset, endOffset);
-    const [numButtons, setNumButtons] = useState(5); 
+    const [numButtons, setNumButtons] = useState(5);
 
     useEffect(() => {
         const handleResize = () => {
@@ -35,7 +34,7 @@ export const NewsPage = () => {
                 setNumButtons(5);
             }
         };
-        
+
         handleResize();
 
         window.addEventListener('resize', handleResize);
@@ -45,42 +44,53 @@ export const NewsPage = () => {
         };
     }, []);
 
-    const handlePageClick = (selectedPage) => {
+    const handlePageClick = selectedPage => {
         const newOffset = selectedPage * cardsPerPage;
-        console.log(
-            `User requested page number ${selectedPage}, which is offset ${newOffset}`
-        );
         setItemOffset(newOffset);
         setActive(selectedPage + 1);
     };
 
     const pageButtons = [];
-    for (let i = 0; i < numButtons; i++) {
-        const pageNumber = itemOffset / cardsPerPage + i + 1;
-        pageButtons.push(
-            <PaginationButton
-                key={pageNumber}
-                onClick={() => handlePageClick(i)}
-                currentButton={i + 1}
-                active={active}
-            >
-                {pageNumber}
-            </PaginationButton>
-        );
-    }
+    const totalPages = Math.ceil(news.length / cardsPerPage);
+
+        let start = active - 2;
+        let end = active + 2;
+
+        if (active < 3) {
+            start = 1;
+            end = numButtons;
+        } else if (active > totalPages - 2) {
+            start = totalPages - numButtons + 1;
+            end = totalPages;
+        }
+
+        for (let i = start; i <= end; i++) {
+            pageButtons.push(
+                <PaginationButton
+                    key={i}
+                    onClick={() => handlePageClick(i - 1)}
+                    currentButton={i}
+                    active={active}
+                >
+                    {i}
+                </PaginationButton>
+            );
+        }
+    
 
     const handlePrevPage = () => {
         if (itemOffset > 0) {
             setItemOffset(itemOffset - cardsPerPage);
+            setActive(active - 1)
         }
     };
 
     const handleNextPage = () => {
         if (itemOffset + cardsPerPage < news.length) {
             setItemOffset(itemOffset + cardsPerPage);
+            setActive(active + 1)
         }
     };
-
     return (
         <NewsWrapper>
             <SearchWrapper>
@@ -113,9 +123,32 @@ export const NewsPage = () => {
                 ))}
             </NewsContainer>
             <PaginationWrapper>
-                <PaginationButton style={{ display: "flex", alignItems: "center", justifyContent: 'center', backgroundColor: "transparent", padding: "10px 8px" }} onClick={handlePrevPage}>{iconRowLeft}</PaginationButton>
+                <PaginationButton
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: 'transparent',
+                        padding: '10px 8px',
+                    }}
+                    onClick={handlePrevPage}
+                >
+                    {iconRowLeft}
+                </PaginationButton>
                 <ListButtonForPagination>{pageButtons}</ListButtonForPagination>
-                <PaginationButton style={{ display: "flex", alignItems: "center", justifyContent: 'center', backgroundColor: "transparent", transform: "rotate(180deg)", padding: "10px 8px" }} onClick={handleNextPage}>{iconRowLeft}</PaginationButton>
+                <PaginationButton
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: 'transparent',
+                        transform: 'rotate(180deg)',
+                        padding: '10px 8px',
+                    }}
+                    onClick={handleNextPage}
+                >
+                    {iconRowLeft}
+                </PaginationButton>
             </PaginationWrapper>
         </NewsWrapper>
     );
